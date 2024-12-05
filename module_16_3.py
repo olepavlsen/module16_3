f342fab09d93635ec4ed1ec80fed55a602b1e5fc
olepavlsen <olepavlsen@mail.ru>
2024-12-05 20:33:15 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -12,8 +12,8 @@ async def get_users() -> dict:
 
 
 @app.post("/user/{username}/{age}")
-async def create_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
-                      , age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> str:
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")]
+                      , age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
     current_user = str(int(max(users, key=int)) + 1)
     users[current_user] = username, age
     return f"User {current_user} is registered"
@@ -22,13 +22,13 @@ async def create_user(username: str = Path(min_length=5, max_length=20, descript
 @app.put("/user/{user_id}/{username}/{age}")
 async def update_user(user_id: int, username: Annotated[
     str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")]
-                      , age: int = Path(ge=18, le=120, description="Enter age", example="24"), ) -> str:
+                      , age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
     users[user_id] = username, age
     return f"The user {user_id} is updated"
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: str) -> str:
+async def delete_user(user_id: Annotated[str, Path(description="Enter user_id")]) -> str:
     users.pop(user_id)
     return f"User {user_id} has been deleted"
 
