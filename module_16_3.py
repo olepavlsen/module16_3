1dd7598ee842495e159153bda05c684e215c22b1
olepavlsen <olepavlsen@mail.ru>
2024-12-05 20:09:03 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -12,15 +12,18 @@ async def get_users() -> dict:
 
 
 @app.post("/user/{username}/{age}")
-async def create_user(user: str) -> str:
+async def create_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
+                      , age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> str:
     current_user = str(int(max(users, key=int)) + 1)
-    users[current_user] = user
+    users[current_user] = username, age
     return f"User {current_user} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, user: str) -> str:
-    users[user_id] = user
+async def update_user(user_id: int, username: Annotated[
+    str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")]
+                      , age: int = Path(ge=18, le=120, description="Enter age", example="24"), ) -> str:
+    users[user_id] = username, age
     return f"The user {user_id} is updated"
 
 
@@ -28,7 +31,5 @@ async def update_user(user_id: str, user: str) -> str:
 async def delete_user(user_id: str) -> str:
     users.pop(user_id)
     return f"User {user_id} has been deleted"
-
-
 
 # uvicorn module_16_3:app
