ab7482517da1274534aabb9f051903ca660c7b62
olepavlsen <olepavlsen@mail.ru>
2024-12-05 22:57:08 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -12,24 +12,24 @@ async def get_users() -> dict:
 
 
 @app.post("/user/{username}/{age}")
-async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")]
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,  description="Enter username", example="UrbanUser", )]
                       , age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
     current_user = str(int(max(users, key=int)) + 1)
-    users[current_user] = username, age
+    users[current_user] = f"Имя: {username}, возраст: {age}"
     return f"User {current_user} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int, username: Annotated[
+async def update_user(user_id: Annotated[int, Path(gt=0, description="Enter user_id")], username: Annotated[
     str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")]
                       , age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
-    users[user_id] = username, age
+    users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: Annotated[str, Path(description="Enter user_id")]) -> str:
-    users.pop(user_id)
+async def delete_user(user_id: Annotated[int, Path(gt=0, description="Enter user_id")]) -> str:
+    users.pop(str(user_id))
     return f"User {user_id} has been deleted"
 
 # uvicorn module_16_3:app
